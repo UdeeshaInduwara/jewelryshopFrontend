25a7a1ecce005fde2ee66cd536c228cf60e2ba82
Udeesha Induwara <udeesha@mi-synergy.com>
2019-08-27 10:20:30 +0530

    Models updated.

--- src/app/models/gem.model.ts
+++ src/app/models/gem.model.ts
@@ -1,7 +1,8 @@
 export class Gem {
-    gemId: number
-    gemType: string
-    carat: number
-    weight: number
-    addedDate: Date
+  gemId: number;
+  gemType: string;
+  carat: number;
+  weight: number;
+  addedDate: Date;
+  givenDate: Date;
 }
