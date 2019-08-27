58a91fa3340e27d833467cdb31d5eb5f3ca4278b
Udeesha Induwara <udeesha@mi-synergy.com>
2019-08-27 09:41:59 +0530

    Created makers saving and delete options.

--- src/app/makers/makers.component.ts
+++ src/app/makers/makers.component.ts
@@ -1,6 +1,6 @@
-import { MakersService } from './../services/makers.service';
-import { Makers } from './../models/makers.model';
-import { Component, OnInit } from '@angular/core';
+import {MakersService} from './../services/makers.service';
+import {Makers} from './../models/makers.model';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-makers',
@@ -9,12 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakersComponent implements OnInit {
   makers: Array<Makers> = [];
+  makersData: Makers = new Makers();
 
-  constructor(private makersService: MakersService) { }
+  constructor(private makersService: MakersService) {
+  }
 
   ngOnInit() {
+    this.getAllMakers();
+  }
+
+  getAllMakers() {
     this.makersService.getAllMakers().subscribe(rst => {
       this.makers = rst;
+    });
+  }
+
+  saveMakers() {
+    this.makersService.saveMakers(this.makersData).subscribe(rst => {
+      console.log(rst);
+    });
+  }
+
+  deleteMakers(id: number) {
+    this.makersService.deleteMakers(id).subscribe(rst => {
+      console.log(rst);
     });
   }
 
