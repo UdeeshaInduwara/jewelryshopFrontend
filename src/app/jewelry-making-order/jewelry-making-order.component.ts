7473abf13e92d24e350eab75afa3001ffa4bd0b2
Udeesha Induwara <udeesha@mi-synergy.com>
2019-08-27 10:48:22 +0530

    Jewelry Making Ordr component created.

---
+++ src/app/jewelry-making-order/jewelry-making-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jewelry-making-order',
+  templateUrl: './jewelry-making-order.component.html',
+  styleUrls: ['./jewelry-making-order.component.scss']
+})
+export class JewelryMakingOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
